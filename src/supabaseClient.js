11afaca544a66247a6b78cc7302bc8806666cf00
Quockhanh <taonichmoi123z@gmail.com>
2025-11-05 08:48:57 +0000

    dong bo

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://rpavicohtzbylqkoudat.supabase.co";
+const SUPABASE_URL = "https://klcdehyiflqemvdknsir.supabase.co";
 const SUPABASE_API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwYXZpY29odHpieWxxa291ZGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3MjU1OTYsImV4cCI6MjA3NzMwMTU5Nn0.pAlMSb2HY1gogijvuZ2JQNN_9CNVoESsYUntO3PPsVk";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsY2RlaHlpZmxxZW12ZGtuc2lyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2MDY4NDcsImV4cCI6MjA3NzE4Mjg0N30.uevV1t8woGbXYK0Mt4wJs8bRWo3QaT_efMY9CZT4Jdg";
 export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
